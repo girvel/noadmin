86707b6289e042ac98f7875c5a42e890faae26b3
girvel <widauka@ya.ru>
2026-03-11 20:10:09 +0500

    Set up the rebuilding process

--- noadmin.c
+++ noadmin.c
@@ -1,28 +1,22 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+
+#define NOB_IMPLEMENTATION
+#define NOB_EXPERIMENTAL_DELETE_OLD
+#define NOB_REBUILD_URSELF(binary_path, source_path) \
+    "cc", "-x", "c", "-Wall", "-Wextra", "-Werror", "-o", binary_path, source_path
+#include "nob.h"
 
 #define C_RED "\x1b[31m"
 #define C_GREEN "\x1b[32m"
 #define C_RESET "\x1b[0m"
 
-void log_info(const char *str) {
-    printf(C_GREEN "[INFO]" C_RESET " %s\n", str);
-}
-
-bool needs_rebuild(const char *source, const char *binary) {
-    struct stat statbuf = {0};
-    if (stat(
-}
-
-void rebuild_itself(const char *binary) {
-    
-    log_info(__FILE__);
-}
-
 int main(int argc, char **argv) {
-    log_info("Initialized file structure");
-    rebuild_itself(argv[0]);
+    nob_minimal_log_level = NOB_ERROR;
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
     printf("  Neovim " C_GREEN "OK\n" C_RESET);
     return 0;
 }
