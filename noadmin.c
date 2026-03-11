61cf0cfd36789a9560484266c10fc3c0885aa153
girvel <widauka@ya.ru>
2026-03-11 21:05:40 +0500

    #

--- noadmin.c
+++ noadmin.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 
 #define NOB_IMPLEMENTATION
@@ -16,6 +17,20 @@
 int main(int argc, char **argv) {
     nob_minimal_log_level = NOB_ERROR;
     NOB_GO_REBUILD_URSELF(argc, argv);
+
+    const char *home = getenv("HOME");
+    if (home == NULL) {
+        fprintf(stderr, "No $HOME found");
+        return 1;
+    }
+
+    printf("  File structure ");
+    if (nob_mkdir_if_not_exists(nob_temp_sprintf("%s/.local/bin", home))) {
+        printf(C_GREEN "OK" C_RESET "\n");
+    } else {
+        printf(C_RED "ERROR" C_RESET "\n");
+        return 1;
+    }
 
     printf("  Neovim " C_GREEN "OK\n" C_RESET);
     return 0;
